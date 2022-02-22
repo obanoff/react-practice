6c404f0e94f20127eb5aae519874f25c077b95ba
obanoff <83809118+obanoff@users.noreply.github.com>
2022-02-22 20:26:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [errorMessage, setErrorMessage] = React.useState('');
 
   const onClickHandler = (e) => {
+    if (e.target !== e.currentTarget) return;
     setState(true);
   };
 
